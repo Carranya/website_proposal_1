7b435508e6548b681599362ee47ea9332926786c
Carranya <k.gong82@gmail.com>
2022-09-09 11:04:20 +0200

    updated

--- index.php
+++ index.php
@@ -2,26 +2,31 @@
 <html lang="de">
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Praxis</title>
     <link rel="stylesheet" href="format.css">
 </head>
 <body>
     <div class='head'>
-        <p>head</p>
+        <div class='head-left'>
+            <img src='img/logo.jpg' style='width: 200px; height: 200px'></img>
+        </div>
+
+        <div class='head-right'>
+            <h1>Karma</h1>
+        </div>
     </div>
 
     <div class='main'>
-        <p>main</p>
-        <input type='checkbox' id='menubox' class='menucheck'>
-        <div class='menu'>
-            <label class='toggle' for='menubox'></label>
-            <ul>
-                <li><a href='#'>Menu1</a></li>
-                <li><a href='#'>Menu2</a></li>
-                <li><a href='#'>Menu3</a></li>
-            </ul>
-        </div>
-    
+            <input type='checkbox' id='menubox' class='menucheck'>
+            <div class='menu'>
+                <label class='toggle' for='menubox'></label>
+                <ul>
+                    <li><a href='#'>Menu1</a></li>
+                    <li><a href='#'>Menu2</a></li>
+                    <li><a href='#'>Menu3</a></li>
+                </ul>
+            </div>
     </div>
     
     <div class='foot'>
