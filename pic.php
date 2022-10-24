362d8a98fe29c2d6dd847823e139ef010e745d71
Carranya <k.gong82@gmail.com>
2022-10-24 08:13:59 +0200

    cleaned up

--- pic.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $img = imagecreatetruecolor(800, 600);
-
-    $color = imagecolorallocate($img, 255, 235, 205);
-        imagefill($img, 0, 0, $color);
-
-    imagejpeg($img, "pic3.jpg");
-    
-?>
